0c15a1b92fd4e0e2533ce7f1d2db93674ae09af0
Jiahui Zhang <jiahui9923@gmail.com>
2023-06-06 18:28:36 -0700

    Modify the front-end code

---
+++ jiahui.js
@@ -0,0 +1,106 @@
+// My Experience
+
+function showuva1(){
+    // window.alert("haha");
+    document.getElementById("showuva1").style.display = "block";
+    document.getElementById("showuva2").style.display = "none";
+    document.getElementById("showuva3").style.display = "none";
+
+    document.getElementById("titleuva1").style.backgroundColor = "#f5f5f5";
+    document.getElementById("titleuva2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("titleuva3").style.backgroundColor = "#FFFFFF";
+}
+
+function showuva2(){
+    document.getElementById("showuva1").style.display = "none";
+    document.getElementById("showuva2").style.display = "block";
+    document.getElementById("showuva3").style.display = "none";
+
+    document.getElementById("titleuva1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("titleuva2").style.backgroundColor = "#f5f5f5";
+    document.getElementById("titleuva3").style.backgroundColor = "#FFFFFF";
+
+}
+
+function showuva3(){
+    document.getElementById("showuva1").style.display = "none";
+    document.getElementById("showuva2").style.display = "none";
+    document.getElementById("showuva3").style.display = "block";
+
+    document.getElementById("titleuva1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("titleuva2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("titleuva3").style.backgroundColor = "#f5f5f5";
+}
+
+function showpsu1(){
+    // window.alert("haha");
+    document.getElementById("showpsu1").style.display = "block";
+    document.getElementById("showpsu2").style.display = "none";
+    document.getElementById("showpsu3").style.display = "none";
+
+    document.getElementById("titlepsu1").style.backgroundColor = "#f5f5f5";
+    document.getElementById("titlepsu2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("titlepsu3").style.backgroundColor = "#FFFFFF";
+    // document.getElementsByClassName("content-template-content-1").style.display="block";
+    // window.alert("hha");
+    // document.getElementsByClassName("content-template-content-2").style.display="none";
+    // document.getElementsByClassName("content-template-content-3").style.display="none";
+}
+
+function showpsu2(){
+    document.getElementById("showpsu1").style.display = "none";
+    document.getElementById("showpsu2").style.display = "block";
+    document.getElementById("showpsu3").style.display = "none";
+
+    document.getElementById("titlepsu1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("titlepsu2").style.backgroundColor = "#f5f5f5";
+    document.getElementById("titlepsu3").style.backgroundColor = "#FFFFFF";
+
+}
+
+function showpsu3(){
+    document.getElementById("showpsu1").style.display = "none";
+    document.getElementById("showpsu2").style.display = "none";
+    document.getElementById("showpsu3").style.display = "block";
+
+    document.getElementById("titlepsu1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("titlepsu2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("titlepsu3").style.backgroundColor = "#f5f5f5";
+}
+
+
+function showccut1(){
+    // window.alert("haha");
+    document.getElementById("showccut1").style.display = "block";
+    document.getElementById("showccut2").style.display = "none";
+    document.getElementById("showccut3").style.display = "none";
+
+    document.getElementById("titleccut1").style.backgroundColor = "#f5f5f5";
+    document.getElementById("titleccut2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("titleccut3").style.backgroundColor = "#FFFFFF";
+    // document.getElementsByClassName("content-template-content-1").style.display="block";
+    // window.alert("hha");
+    // document.getElementsByClassName("content-template-content-2").style.display="none";
+    // document.getElementsByClassName("content-template-content-3").style.display="none";
+}
+
+function showccut2(){
+    document.getElementById("showccut1").style.display = "none";
+    document.getElementById("showccut2").style.display = "block";
+    document.getElementById("showccut3").style.display = "none";
+
+    document.getElementById("titleccut1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("titleccut2").style.backgroundColor = "#f5f5f5";
+    document.getElementById("titleccut3").style.backgroundColor = "#FFFFFF";
+
+}
+
+function showccut3(){
+    document.getElementById("showccut1").style.display = "none";
+    document.getElementById("showccut2").style.display = "none";
+    document.getElementById("showccut3").style.display = "block";
+
+    document.getElementById("titleccut1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("titleccut2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("titleccut3").style.backgroundColor = "#f5f5f5";
+}
